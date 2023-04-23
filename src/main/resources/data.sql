b96bc6dc4a41c0b4c865244a0372cddc22ec0805
Sergey Bondarev <bondarevsy163@gmail.com>
2023-04-23 12:31:16 +0400

    create data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,6 +4,11 @@ insert into user(login, password, role) values
                                             ('admin',
                                              '$2a$12$I2y09CJuNkq/uOox85dfBOwT6iSzCJo8FomE/E8sEIuH7g1XwHFTm',
                                              'ROLE_ADMIN');
+-- учебное заведение
+insert into university(name) values ('University');
+-- два общежития по 50 койко-мест
+insert into residence(capacity, gender, university_id) VALUES (50, 'FEMALE', 1);
+insert into residence(capacity, gender, university_id) VALUES (50, 'MALE', 1);
 
 
 
